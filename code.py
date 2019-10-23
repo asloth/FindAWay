c1439c81b94b2f57d711f90af2b3ade59133dff3
Sara Benel <sbenelramirez@gmail.com>
2019-10-23 12:08:09 -0500

    algunos cambios

--- code.py
+++ code.py
@@ -9,34 +9,32 @@ Created on Thu Oct  3 19:53:18 2019
 
 # Networkx para grafos
 import networkx as nx
-
 # Pandas
 import pandas as pd
-
 # Mathplotlib
 import matplotlib.pyplot as plt
 
 plt.rcParams['figure.figsize'] = (20.0, 10.0)
 
-iata_spain = pd.read_csv('vertex.csv')
+cix = pd.read_csv('vertices.csv')
 
-iata_spain.set_index(["code"], inplace=True)
-iata_spain.head()
+cix.set_index(["code"], inplace=True)
+cix.head()
 
-spain_flights = pd.read_csv("rutas.csv")
-spain_flights.head()
+rutas_cix = pd.read_csv("rutascompletas.csv")
+rutas_cix.head()
 
 DG=nx.DiGraph()
-for row in spain_flights.iterrows():
+for row in rutas_cix.iterrows():
     DG.add_edge(row[1]["origin"],
                 row[1]["destination"],
                 duration=row[1]["cost"])
-    
+
 DG.nodes(data=True)
 
-nx.draw(DG, 
+nx.draw_circular(DG, 
                  node_color="lightblue",
                  edge_color="gray",
-                 font_size=24,
+                 font_size=20,
                  width=2, with_labels=True, node_size=3500,
 )
